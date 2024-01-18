d54edf6176e8777f7e47165bbc0a262688992814
Senk0 <francoorlando11@gmail.com>
2024-01-18 21:35:40 +0100

    FirstCommit

---
+++ portfolio/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name= "Titulo")
+    description =  models.TextField(verbose_name= "Descripción")
+    image = models.ImageField(verbose_name= "Imagen", upload_to="projects")
+    url = models.URLField(null=True, blank=True)
+    created =  models.DateTimeField(auto_now_add=True, verbose_name= "Fecha de Creación")
+    updated =  models.DateTimeField(auto_now=True, verbose_name= "Fecha de Modificación")
+
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
+
+# cambiar titulo de proyectos
+    def __str__(self):
+        return self.title
